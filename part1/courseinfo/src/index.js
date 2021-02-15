4f4e6af719ca9e01f6bf04eaf398db9b766710d2
Ruel Neuman <ruelneuman@gmail.com>
2021-02-15 11:42:53 -0800

    courseinfo: refactor variable definitions as per ex. 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
@@ -26,9 +32,9 @@ const Header = (props) => {
 const Content = (props) => { 
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
+      <Part part={props.part1.name} exercises={props.part1.exercises}/>
+      <Part part={props.part2.name} exercises={props.part2.exercises}/>
+      <Part part={props.part3.name} exercises={props.part3.exercises}/>
     </div>
   )
 }
