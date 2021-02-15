5892d713ba454b318f65088b3ef0b18e1574357a
Ruel Neuman <ruelneuman@gmail.com>
2021-02-15 12:34:53 -0800

    courseinfo: refactor with destructuring and as per ex. 1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,54 +2,56 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
-const Header = (props) => {
-  return (<h1>{props.course}</h1>);
+const Header = ({ course }) => {
+  return (<h1>{course}</h1>);
 }
 
-const Content = (props) => { 
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={props.parts[0]}/>
-      <Part part={props.parts[1]}/>
-      <Part part={props.parts[2]}/>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </div>
   )
 }
 
-const Total = (props) => {
-  return (<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>);
+const Total = ({ parts }) => {
+  return (<p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>);
 }
 
-const Part = (props) => {
+const Part = ({ part }) => {
   return (
-   <p>
-     {props.part.name} {props.part.exercises}
-   </p>
+    <p>
+      {part.name} {part.exercises}
+    </p>
   )
 }
 
