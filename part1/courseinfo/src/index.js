2e76eafce9f144ab3364fb6877e5342c292570d0
Ruel Neuman <ruelneuman@gmail.com>
2021-02-15 11:05:09 -0800

    courseinfo: refactor Content as per ex. 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -26,21 +26,23 @@ const Header = (props) => {
 const Content = (props) => { 
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
     </div>
   )
 }
 
 const Total = (props) => {
   return (<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>);
+}
+
+const Part = (props) => {
+  return (
+   <p>
+     {props.part} {props.exercises}
+   </p>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
