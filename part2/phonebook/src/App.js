49a8b0e2ae434f8ea73706d0bd5656b9645f02a1
Ruel Neuman <ruelneuman@gmail.com>
2021-02-21 15:41:30 -0800

    phonebook: add number input field as per ex 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -14,13 +13,18 @@ const App = () => {
             return;
         }
 
-        const newPerson = { name: newName };
+        const newPerson = { name: newName, number: newNumber };
         setPersons(persons.concat(newPerson));
         setNewName('');
+        setNewNumber('');
     }
 
-    const handleChange = (event) => {
+    const handleNameChange = (event) => {
         setNewName(event.target.value);
+    }
+
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value);
     }
 
     return (
@@ -28,7 +32,18 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    name: <input value={newName} onChange={handleChange} />
+                    name:
+                    <input
+                        value={newName}
+                        onChange={handleNameChange}
+                    />
+                </div>
+                <div>
+                    number:
+                    <input
+                        value={newNumber}
+                        onChange={handleNumberChange}
+                    />
                 </div>
                 <div>
                     <button type="submit">add</button>
@@ -36,9 +51,9 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             {persons.map(person =>
-                <p key={person.name}>
-                    {person.name}
-                </p>
+                <div key={person.name}>
+                    {person.name} {person.number}
+                </div>
             )}
         </div>
     );
