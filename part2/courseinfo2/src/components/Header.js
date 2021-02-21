66ea1fe6150e7b457273442c1a4ec18b69d6bef0
Ruel Neuman <ruelneuman@gmail.com>
2021-02-21 12:26:57 -0800

    courseinfo2: expand functionality to allow for multiple courses as per ex. 2.4-2.5

--- part2/courseinfo2/src/components/Header.js
+++ part2/courseinfo2/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ course }) => {
     return (
-        <h1>{course.name}</h1>
+        <h2>{course.name}</h2>
     );
 }
 
--- part2/courseinfo2/src/components/Total.js
+++ part2/courseinfo2/src/components/Total.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Total = ({ course: { parts } }) => {
     const total = parts.reduce((sum, part) => sum + part.exercises, 0);
     return (
-        <p>total of {total} exercises</p>
+        <p><strong>total of {total} exercises</strong></p>
     );
 }
 
