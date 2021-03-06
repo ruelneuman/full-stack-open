69bb40963a01209815b89ae80ead45766cfe8a95
Ruel Neuman <ruelneuman@gmail.com>
2021-03-05 22:02:34 -0800

    phonebook-backend: added info functionality as per ex. 3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
     response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+    response.send(
+        `<div>The phonebook has info for ${persons.length} people.</div>
+         <div>${new Date()}</div>`
+    );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
