458e51fb8e0f8860dd7fd5aaeff1e40c8ee9cd49
Ruel Neuman <ruelneuman@gmail.com>
2021-03-06 10:04:52 -0800

    phonebook-backend: show request body in morgan log as per ex. 3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,12 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('body', (request, response, next) => {
+    return JSON.stringify(request.body);
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
     {
