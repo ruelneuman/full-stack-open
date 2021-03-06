ebf0f4e0b6e83eaa5cac75bb8534b7880d04152a
Ruel Neuman <ruelneuman@gmail.com>
2021-03-05 21:46:55 -0800

    phonebook-backend: return persons list as per ex. 3.1

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122",
+    },
+];
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
