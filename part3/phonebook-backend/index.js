d57eac8ec7da9ddc8b6aa24825a9be5f8d46d451
Ruel Neuman <ruelneuman@gmail.com>
2021-03-05 22:21:36 -0800

    phonebook-backend: implement functionality for displaying a single person as per ex. 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
     {
         id: 1,
@@ -24,15 +26,26 @@ let persons = [
     },
 ];
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons);
-});
-
 app.get('/info', (request, response) => {
     response.send(
         `<div>The phonebook has info for ${persons.length} people.</div>
          <div>${new Date()}</div>`
     );
+});
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
+
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
 });
 
 const PORT = 3001;
