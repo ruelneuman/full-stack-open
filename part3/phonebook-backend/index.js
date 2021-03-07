3f2002b5646126159c90966a108e3b5d12672d53
Ruel Neuman <ruelneuman@gmail.com>
2021-03-07 13:47:32 -0800

    phonebook-backend: configure to deploy frontend to heroku as per ex. 3.10

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -105,7 +105,7 @@ app.post('/api/persons/', (request, response) => {
     response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
