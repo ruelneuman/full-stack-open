46e9de90bb822515ad7713ea10f537d4e4fe256c
Ruel Neuman <ruelneuman@gmail.com>
2021-03-07 14:46:50 -0800

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('body', (request, response, next) => {
     return JSON.stringify(request.body);
