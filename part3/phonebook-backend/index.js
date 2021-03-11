06d24c93ef354734b0911f86287f9a0428a8d67e
Ruel Neuman <ruelneuman@gmail.com>
2021-03-10 21:48:08 -0800

    phonebook-backend: support PUT request as per ex. 3.17

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -72,6 +72,22 @@ app.post('/api/persons/', (request, response, next) => {
         .catch((error) => next(error));
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    };
+
+    Person
+        .findByIdAndUpdate(request.params.id, person, { new: true })
+        .then((updatedPerson) => {
+            response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
+});
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' });
 };
