97e9ece6b504bbd1fce122e3dbc1230754ee4c37
Ruel Neuman <ruelneuman@gmail.com>
2021-03-12 21:35:05 -0800

    phonebook-backend: add unique validator as per ex. 3.19

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -103,7 +103,10 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message);
 
     if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+    if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message });
     }
 
     next(error);
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI;
 
@@ -17,9 +18,15 @@ mongoose
     });
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     number: String,
 });
+
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
