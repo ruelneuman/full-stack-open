a25f5c79dec8c1b05b9a0b6a2d439826ad6d9220
Ruel Neuman <ruelneuman@gmail.com>
2021-03-13 18:09:30 -0800

    blog-list-backend: add totalLikes function and tests as per ex. 4.4

--- part4/blog-list-backend/utils/list_helper.js
+++ part4/blog-list-backend/utils/list_helper.js
@@ -1,7 +1,14 @@
-const dummy = (blogs) => {
-  // ...
+const dummy = () => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => {
+    return total + blog.likes;
+  }, 0);
 };
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
