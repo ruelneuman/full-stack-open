22350addcdca18e79b232c30679387b9f332b2fa
Ruel Neuman <ruelneuman@gmail.com>
2021-03-13 19:09:35 -0800

    blog-list-backend: add favoriteBlog function and tests as per ex. 4.5

--- part4/blog-list-backend/utils/list_helper.js
+++ part4/blog-list-backend/utils/list_helper.js
@@ -3,12 +3,27 @@ const dummy = () => {
 };
 
 const totalLikes = (blogs) => {
-  return blogs.reduce((total, blog) => {
-    return total + blog.likes;
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes;
   }, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const favoriteBlog = blogs.reduce((mostLikedBlog, blog) => {
+    return (mostLikedBlog.likes > blog.likes) ? mostLikedBlog : blog;
+  }, blogs[0]);
+
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes
+  };
 };
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
