2accf1493ecef15201ed98f99c56476a5b225596
Ruel Neuman <ruelneuman@gmail.com>
2021-03-20 13:32:33 -0700

    blog-list-backend: add db functionality for users as per ex. 4.15

--- part4/blog-list-backend/app.js
+++ part4/blog-list-backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('express-async-errors');
 const app = express();
 const cors = require('cors');
+const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
@@ -27,6 +28,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
 app.use(middleware.unknownEndpoint);
