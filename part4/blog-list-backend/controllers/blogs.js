204eb96f7d8f56bd6f0c42555e0c8d16e4182820
Ruel Neuman <ruelneuman@gmail.com>
2021-03-22 20:47:04 -0700

    blog-list-backend: prevent blogs from being posted without valid token as per ex. 4.19

--- part4/blog-list-backend/controllers/blogs.js
+++ part4/blog-list-backend/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
@@ -22,9 +23,24 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization?.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
-  const user = await User.findOne({});
+  const token = getTokenFrom(request);
+  const decodedToken =jwt.verify(token, process.env.SECRET);
+
+  if(!token || !decodedToken.id ) {
+    return response.status(401).json({error: 'token missing or invalid'});
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
--- part4/blog-list-backend/utils/middleware.js
+++ part4/blog-list-backend/utils/middleware.js
@@ -23,6 +23,14 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message });
   }
 
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' });
+  }
+
+  if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' });
+  }
+
   next(error);
 };
 
