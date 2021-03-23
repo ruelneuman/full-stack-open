8f1fd4494f9060edd13a40c6f7d5a5ccf7829ca4
Ruel Neuman <ruelneuman@gmail.com>
2021-03-22 20:10:05 -0700

    blog-list-backend: implement token-based authenticiation as per ex. 4.18

--- part4/blog-list-backend/app.js
+++ part4/blog-list-backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('express-async-errors');
 const app = express();
 const cors = require('cors');
+const loginRouter = require('./controllers/login');
 const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 const middleware = require('./utils/middleware');
@@ -28,6 +29,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
