c7a0cfbc2201efe179e94a812d6387e5a92f49ca
Ruel Neuman <ruelneuman@gmail.com>
2021-04-03 15:30:51 -0700

    blog-list-frontend: remove useRef from BlogList component

--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -1,16 +1,14 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Blog from './Blog';
 import AddBlogForm from './AddBlogForm';
 import Togglable from './Togglable';
 
 const BlogList = ({ blogs, addBlog }) => {
-  const addBlogFormRef = useRef(null);
-
   return (
     <div>
       <h1>Blogs</h1>
-      <Togglable buttonLabel="Add blog" ref={addBlogFormRef}>
-        <AddBlogForm addBlog={addBlog} addBlogFormRef={addBlogFormRef} />
+      <Togglable buttonLabel="Add blog" >
+        <AddBlogForm addBlog={addBlog} />
       </Togglable>
       <div>
         {blogs.map(blog =>
