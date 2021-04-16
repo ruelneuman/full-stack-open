c6564ec0f0bad64c7963933c38de6526cecd8767
Ruel Neuman <ruelneuman@gmail.com>
2021-04-16 12:49:44 -0700

    redux-anecdotes: create AnecdoteList component as per ex. 6.8

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import AnecdoteForm from './components/AnecdoteForm';
-import { voteAnecdote } from './reducers/anecdoteReducer';
+import AnecdoteList from './components/AnecdoteList';
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    dispatch(voteAnecdote(id));
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
+      <AnecdoteList />
+      <h2>Create New</h2>
       <AnecdoteForm />
     </div>
   );
