e545c79f2e53ec32c83d053764467080c00652fe
Ruel Neuman <ruelneuman@gmail.com>
2021-05-16 11:15:39 -0700

    library-backend: implement db functionality as per ex. 8.13

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,83 +1,16 @@
+const { MONGODB_URI } = require('./utils/config');
 const { ApolloServer, gql } = require('apollo-server');
-const { v1: uuid } = require('uuid');
+const mongoose = require('mongoose');
+const Book = require('./models/book');
+const Author = require('./models/author');
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-    born: 1821
-  },
-  {
-    name: 'Joshua Kerievsky',
-    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-  },
-  {
-    name: 'Sandi Metz',
-    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-  },
-];
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'patterns']
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'revolution']
-  },
-];
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
 
 const typeDefs = gql`
   type Author {
@@ -88,7 +21,7 @@ const typeDefs = gql`
   }
   type Book {
     title: String!
-    author: String!
+    author: Author!
     published: Int!
     genres: [String!]!
     id: ID!
@@ -118,62 +51,66 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
+    bookCount: () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
     allBooks: (root, { author, genre }) => {
-      if (!author && !genre) return books;
+      if (!author && !genre) return Book.find({}).populate('author');
 
-      const filteredBooks = books.filter((book) => {
-        if (author && book.author !== author) return false;
+      // IMPLEMENT FILTER LIKE BELOW, BUT WITH MONGODB:
+      // const filteredBooks = books.filter((book) => {
+      //   if (author && book.author !== author) return false;
 
-        if (genre && !book.genres.includes(genre)) return false;
+      //   if (genre && !book.genres.includes(genre)) return false;
 
-        return true;
-      });
+      //   return true;
+      // });
 
-      return filteredBooks;
+      // return filteredBooks;
     },
-    allAuthors: () => authors,
+    allAuthors: () => Author.find({}),
   },
   Author: {
-    bookCount: (root) => {
-      const bookCount = books.reduce((count, book) => {
-        return book.author === root.name
-          ? count + 1
-          : count;
-      }, 0);
-      return bookCount;
-    }
+    // IMPLEMENT TO WORK WITH DB:
+    // bookCount: (root) => {
+    //   const bookCount = books.reduce((count, book) => {
+    //     return book.author === root.name
+    //       ? count + 1
+    //       : count;
+    //   }, 0);
+    //   return bookCount;
+    // }
   },
   Mutation: {
-    addBook: (root, args) => {
-      const book = { ...args, id: uuid() };
-      books = books.concat(book);
+    addBook: async (root, args) => {
+      const name = args.author;
+      let author = await Author.findOne({ name });
 
-      const authorExists = authors.some((author) => author.name === args.author);
-
-      if (!authorExists) {
-        const author = { name: args.author, id: uuid() };
-        authors = authors.concat(author);
+      if (!author) {
+        author = new Author({ name });
+        await author.save();
       }
 
-      return book;
+      const book = new Book({ ...args, author: author._id });
+      await book.save();
+
+      return await book.populate('author').execPopulate();
     },
-    editAuthor: (root, { name, setBornTo }) => {
-      const author = authors.find((author) => author.name === name);
+    // IMPLEMENT TO WORK WITH DB:
+    // editAuthor: (root, { name, setBornTo }) => {
+    //   const author = authors.find((author) => author.name === name);
 
-      if (!author) return null;
+    //   if (!author) return null;
 
-      const updatedAuthor = { ...author, born: setBornTo };
+    //   const updatedAuthor = { ...author, born: setBornTo };
 
-      authors = authors.map((author) => {
-        return author.id !== updatedAuthor.id
-          ? author
-          : updatedAuthor;
-      });
+    //   authors = authors.map((author) => {
+    //     return author.id !== updatedAuthor.id
+    //       ? author
+    //       : updatedAuthor;
+    //   });
 
-      return updatedAuthor;
-    }
+    //   return updatedAuthor;
+    // },
   }
 };
 
---
+++ part8/library-backend/models/book.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 2
+  },
+  published: {
+    type: Number,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  },
+  genres: [
+    { type: String }
+  ]
+});
+
+schema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('Book', schema);
