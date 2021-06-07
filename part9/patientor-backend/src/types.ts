d6801e5a743532580a28f5458505c188eb507f38
Ruel Neuman <ruelneuman@gmail.com>
2021-06-06 18:51:01 -0700

    patientor-backend: expand Entry type to conform with supplied data as per ex. 9.19

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,18 +1,66 @@
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other"
+}
+
+export enum EntryType {
+  Hospital = "Hospital",
+  OccupationalHealthCare = "OccupationHealthcare",
+  HealthCheck = "HealthCheck"  
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
 export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
 }
 
-export enum Gender {
-  Male = 'male',
-  Female = 'female',
-  Other = 'other'
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
 }
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
